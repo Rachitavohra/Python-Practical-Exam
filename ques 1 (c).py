9f1c762b42b845880d7cd6f3305ab419623881ff
Rachitavohra <101381058+Rachitavohra@users.noreply.github.com>
2022-03-11 12:16:30 +0530

    Add files via upload

---
+++ ques 1 (c).py
@@ -0,0 +1,13 @@
+#sum of n natural numbers
+num = 10
+
+
+if num < 0:
+   print("Enter a positive number")
+else:
+   sum = 0
+   # use while loop to iterate until zero
+   while(num > 0):
+       sum += num
+       num -= 1
+   print("The sum is", sum)
